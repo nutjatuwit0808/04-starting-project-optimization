824f96e2b38fbc3896b00460dca9f4c80229a1b5
nutjatuwit <103878796+nutjatuwit0808@users.noreply.github.com>
2024-11-07 16:17:36 +0700

    initial project

---
+++ components/header.js
@@ -0,0 +1,34 @@
+import logo from "@/assets/logo.png";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Header() {
+  console.log(logo);
+
+  return (
+    <header id="main-header">
+      <Link href="/">
+        <Image
+          width={100}
+          height={100}
+          // sizes="10vw"
+          priority
+          src={logo.src}
+          alt="Mobile phone with posts feed on it"
+        />
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/feed">Feed</Link>
+          </li>
+          <li>
+            <Link className="cta-link" href="/new-post">
+              New Post
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
